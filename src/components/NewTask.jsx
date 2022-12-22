022e3d2628855af1caa2c8db3c129e71aa6f889f
skynetpanda9 <akashpandya18@outlook.com>
2022-12-22 10:55:50 +0530

    added error prompt

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -5,6 +5,7 @@ import { v4 as uuid } from "uuid";
 
 const NewTask = ({ dataInit, setNewData, category, onClose }) => {
   const date = new Date();
+  const [error, setError] = useState(false);
   const [form, setForm] = useState({
     id: uuid(),
     Task: "",
@@ -24,7 +25,7 @@ const NewTask = ({ dataInit, setNewData, category, onClose }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     if (form.Task === "") {
-      alert(`Task shouldn't be empty!`);
+      setError(true);
     } else {
       let newData = insertObject(dataInit, form);
       setNewData(newData);
@@ -32,24 +33,35 @@ const NewTask = ({ dataInit, setNewData, category, onClose }) => {
     }
   };
 
+  const Error = () => {
+    return (
+      <div className='mt-2 mb-0 px-2 text-red-600'>
+        Task shouldn't be empty!
+      </div>
+    );
+  };
+
   return (
     <div>
-      <form className='flex flex-row w-[100%] mt-4' onSubmit={handleSubmit}>
-        <input
-          className='flex flex-row rounded-md w-[80%] p-4 border-none text-base bg-gray-300 dark:bg-gray-700 text-gray-800 dark:text-gray-300 focus:outline-none focus:bg-gray-300 focus:text-gray-900'
-          value={form.Task}
-          name='Task'
-          type='text'
-          autoComplete='off'
-          onChange={handleInput}
-          placeholder='Add Task'
-        />
-        <button
-          className='block w-[20%] ml-2 rounded-md border-none text-gray-300 bg-gray-900 shadow-md hover:bg-green-700'
-          type='submit'
-        >
-          <FontAwesomeIcon icon={faCheck} />
-        </button>
+      <form className='flex flex-col w-[100%] mt-4' onSubmit={handleSubmit}>
+        <div className='flex flex-row'>
+          <input
+            className='flex flex-row rounded-md w-[80%] p-4 border-none text-base bg-gray-300 dark:bg-gray-700 text-gray-800 dark:text-gray-300 focus:outline-none focus:bg-gray-300 focus:text-gray-900'
+            value={form.Task}
+            name='Task'
+            type='text'
+            autoComplete='off'
+            onChange={handleInput}
+            placeholder='Add Task'
+          />
+          <button
+            className='block w-[20%] ml-2 rounded-md border-none text-gray-300 bg-gray-900 shadow-md hover:bg-green-700'
+            type='submit'
+          >
+            <FontAwesomeIcon icon={faCheck} />
+          </button>
+        </div>
+        {error && <Error />}
       </form>
     </div>
   );
